0146be34e696d33fe8943757731e2cfbb2b31d02
pepperonee13 <szarvaspeter13+github@gmail.com>
2025-07-18 12:57:41 +0200

    chore: changed test input

--- kata-1/fsharp/TimeToActParser/Program.fs
+++ kata-1/fsharp/TimeToActParser/Program.fs
@@ -4,14 +4,14 @@ open TimeToActParser.JsonSerializer
 [<EntryPoint>]
 let main argv =
     let input = """<list kind=".">
-1. First
-First body
-2. Second
-Some more text
-<dict sep=":">
-Key: Value
-Another Key: Another Value
-</dict>
+1. Beginning
+2. Main 
+2.1. Subsection
+<list kind="*">
+* Bullet 1
+* Bullet 2
+</list>
+3. Ending
 </list>"""
     
     let result = parseDocument input
